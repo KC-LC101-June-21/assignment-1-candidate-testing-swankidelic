35e5484621fb09a9a8250abdb1339d75ca327cd0
bradgravett <>
2021-06-03 04:37:22 +0000

    Completed Part 1 of the assignment - Minimum Viable Quiz (also added arrays for questions and answers)

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,29 +5,45 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array \n[8, 'Orbit', 'Trajectory', 45]\n what entry is at index 2? ";
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
 let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Hello! What name should be on your answers? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer === correctAnswer) {
+    console.log("Awesome! That's correct!");
+  } else {
+    console.log("Nope, that doesn't match the correct answer. Sorry!");
+  }
 
   let grade;
   
