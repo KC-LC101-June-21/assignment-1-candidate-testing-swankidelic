7132c7d75dbd6027afe32ddc0b7d90d887ab69fd
bradgravett <>
2021-06-03 15:13:14 +0000

    Added TODO 1.1c

--- candidate-testing.js
+++ candidate-testing.js
@@ -42,7 +42,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Welcome, " + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
