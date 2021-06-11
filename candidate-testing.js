3aaf61a11008ccfac050dbdd5cf3e3b21cb4cff0
bradgravett <swankidelic@gmail.com>
2021-06-11 02:51:37 +0000

    Completed and tested assignment. I'm completing this ahead of schedule, as I feel I have the skills for it now, and the step-by-step pace of this assignment is a little confusing and frustrating for me.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,21 @@ let candidateName;
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3",
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,20 +32,42 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-
+  // candidateAnswer = input.question(question);
+  for (let i = 0; i < questions.length + 1; i++){
+    candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log("Awesome! That's correct!");
-  } else {
-    console.log("Nope, that doesn't match the correct answer. Sorry!");
+  // if (candidateAnswer === correctAnswer) {
+  //   console.log("Awesome! That's correct!");
+  // } else {
+  //   console.log("Nope, that doesn't match the correct answer. Sorry!");
+  // }
+
+  let candidateScore = 0;
+  console.log(`Candidate Name: ${candidateName}`);
+
+  for (i = 0; i < questions.length; i++){
+    console.log(`${i + 1}) ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+
+    //NOTE: I used the toLowerCase methods here to make the answers case-insensitive
+    //without altering the original arrays -BWG
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {candidateScore++;}
   }
 
-  let grade;
+  let grade = (candidateScore / questions.length) * 100;
+
+  console.log(`>>> Overall Grade: ${grade}% (${candidateScore} of ${questions.length} correct) <<<`);
+  if (grade >= 80) {
+    console.log(`>>> Status: Pass <<<`); 
+  } else {
+    console.log(`>>> Status: FAILED <<<`);
+  }
   
 
   return grade;
